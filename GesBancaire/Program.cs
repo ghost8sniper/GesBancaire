a0408d9dd34eb290296be03cb2d5b06b586bde68
OUFRID Youness <youness.oufrid1@gmail.com>
2019-03-29 20:57:48 +0100

    test

--- GesBancaire/Program.cs
+++ GesBancaire/Program.cs
@@ -16,7 +16,7 @@ namespace GesBancaire
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FrmOperations());
+            Application.Run(new FrmOperations()); 
         }
     }
 }
